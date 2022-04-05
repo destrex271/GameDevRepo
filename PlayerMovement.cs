58e1aa78433103d171d7735e09cd300290f6e2aa
Akshat Jaimini <destrex271@gmail.com>
2022-04-06 00:45:42 +0530

    PlayerController

---
+++ PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 20.0f;
+
+    private float xBound = 12.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = transform.position.x;
+        if (posX < -xBound || posX > xBound)
+        {
+            int sign = (int) (posX / Math.Abs(posX));
+            transform.position = new Vector3(sign * posX, transform.position.y, transform.position.z);
+        }
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontal * Time.deltaTime * speed);
+    }
+}
