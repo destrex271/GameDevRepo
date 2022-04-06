6e3c08ed6bb5726cc278c2abc7ee4e79f21ff365
Akshat Jaimini <destrex271@gmail.com>
2022-04-07 01:21:46 +0530

    Animal Movement and spawn manager

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public GameObject[] animalPrefabs;
+    private float timeInterval = 1.0f;
+    private float time = 1.0f;
+    private float zSpawn = 16;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        int index = Random.Range(0, 3);
+        int animalX = Random.Range(-13, 13);
+        if (time <= 0)
+        {
+            Instantiate(animalPrefabs[index], new Vector3(animalX, transform.position.y, zSpawn), animalPrefabs[index].transform.rotation);
+            time = timeInterval;
+        }
+        else
+        {
+            time -= Time.deltaTime;
+        }
     }
 }
